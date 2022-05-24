533e5ace356fb9c038d5831936c8fc56b76904c0
Nainik Mehta <nainik.m@crestinfosystems.net>
2022-05-24 11:48:43 +0530

    Match Operation

--- tests/myFirstTest.test.js
+++ tests/myFirstTest.test.js
@@ -36,4 +36,16 @@ describe("Testing other matcher methods", () => {
         expect(a + b).toBeLessThanOrEqual(5);
     });
 
+    test("There should be no I in team", () => {
+        const string = 'team';
+
+        expect(string).not.toMatch(/I/);
+    });
+
+    test("There is 'stop' in Christopher", () => {
+        const string = 'Christopher';
+
+        expect(string).toMatch(/stop/);
+    });
+
 });
