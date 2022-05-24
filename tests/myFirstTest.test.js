45c5088ee31bd433ce079ad3f937b875b0d28332
Nainik Mehta <nainik.m@crestinfosystems.net>
2022-05-24 12:20:58 +0530

    Array equality testing

--- tests/myFirstTest.test.js
+++ tests/myFirstTest.test.js
@@ -37,15 +37,79 @@ describe("Testing other matcher methods", () => {
     });
 
     test("There should be no I in team", () => {
-        const string = 'team';
+        const string = "team";
 
         expect(string).not.toMatch(/I/);
     });
 
     test("There is 'stop' in Christopher", () => {
-        const string = 'Christopher';
+        const string = "Christopher";
 
         expect(string).toMatch(/stop/);
     });
 
+    const shopList = ["Milk", "Trash bags", "Bread", "Eggs", "Cheese", "Butter"];
+
+    test("The Shop list doen't contain PS4", () => {
+        expect(shopList).not.toContain("PS4");
+        expect(shopList).toContain("Milk");
+    });
+});
+
+//  Testing primitive and reference types equality
+describe("Testing reference equality", () => {
+    const user = { name: "Nainik" };
+    user["age"] = 23;
+
+    test("Should return a user object with age as 23", () => {
+        expect(user).toEqual({ name: "Nainik", age: 23 });
+    });
+    test("Should return a user with a name and age key", () => {
+        expect(user).toEqual(
+            expect.objectContaining({
+                name: expect.any(String),
+                age: expect.any(Number),
+            })
+        );
+    });
+
+    // Testing Array Equality
+    test("Array Equality", () => {
+        const users = ["A", "B", "C"];
+        users.push("D");
+
+        expect(users).toEqual(["A", "B", "C", "D"]);
+        expect(users).toEqual(expect.arrayContaining(["D"]));
+        expect(users).toEqual(expect.arrayContaining([expect.any(String)]));
+
+        const userObjectInArray = [
+            {
+                user: "A",
+                age: 23,
+            },
+            {
+                user: "B",
+                age: 24,
+            },
+            {
+                user: "C",
+                age: 23,
+            },
+            {
+                user: "D",
+                age: 21,
+            },
+        ];
+
+        userObjectInArray.push({ user: "E", age: 21 });
+
+        expect(userObjectInArray).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    user: expect.any(String),
+                    age: expect.any(Number),
+                })
+            ])
+        )
+    });
 });
